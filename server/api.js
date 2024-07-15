3b987e56694c08568d539365e62831c01bca9467
RbAvci <rb.avci@gmail.com>
2024-07-15 22:12:43 +0100

    implemented api for users

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import logger from "./utils/logger";
+import db from "./db";
 
 const router = Router();
 
@@ -8,5 +9,36 @@ router.get("/", (_, res) => {
 	logger.debug("Welcoming everyone...");
 	res.json({ message: "Hello, world!" });
 });
+
+
+router.post("/users", async (req, res) => {
+	const { username, passwordHash, userType } = req.body;
+
+	if (!username) {
+		return res.status(422).json({ message: "Username field is required" });
+	}
+	if (!passwordHash) {
+		return res.status(422).json({ message: "Password_hash field is required"});
+	}
+	if (!userType) {
+		return res.status(422).json({ message: "User_type field is required" });
+	}
+
+	try {
+		const result = await db.query(
+			"INSERT INTO users (username, password_hash, user_type) VALUES ($1, $2, $3) RETURNING id",
+			[username, passwordHash, userType]
+		);
+
+		const newUserID = result.rows[0].id;
+		res.status(200).json({ success: true, data: { id: newUserID } });
+	} catch (error) {
+		res
+			.status(500)
+			.json({ success: false, error: "Failed to create a new User into database" });
+	}
+});
+
+
 
 export default router;
