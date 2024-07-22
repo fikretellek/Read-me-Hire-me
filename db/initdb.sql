5b58b94851111ae08fb35f1e45bf768efe0fa1bc
fikretellek <fikretellek@icloud.com>
2024-07-22 23:26:45 +0100

    portfolio table added

--- db/initdb.sql
+++ db/initdb.sql
@@ -1,6 +1,7 @@
 \c readme_hireme_db;
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS portfolios CASCADE;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
@@ -9,4 +10,14 @@ CREATE TABLE users (
     user_type VARCHAR
 );
 
+CREATE TABLE portfolios (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    readme TEXT,
+    activity TEXT,
+    projects TEXT
+);
+
 INSERT INTO users (username, password_hash, user_type) VALUES ('testUser@gmail.com', 'pass_hash', 'graduate');
+INSERT INTO users (username, password_hash, user_type) VALUES ('testUser1@gmail.com', 'pass_hash', 'mentor');
+INSERT INTO users (username, password_hash, user_type) VALUES ('testUser2@gmail.com', 'pass_hash', 'requiter');
