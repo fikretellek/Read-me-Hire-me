53e7db9ba95cdd1b8219818682b0154ad46c0450
fikretellek <fikretellek@icloud.com>
2024-07-23 21:15:11 +0100

    fetching readme function implemented

--- server/functions/fetchReadme.js
+++ server/functions/fetchReadme.js
@@ -1,17 +1,42 @@
-async function fetchReadme (username) {
-    const readmeLink = `https://api.github.com/repos/${username}/${username}/readme`
+import db from "../db";
 
-try {
-    const encodedReadme = (await fetch(readmeLink).then(response => response.json())).content
+export default async function fetchReadme(username) {
+    const readmeLink = `https://api.github.com/repos/${username}/${username}/readme`;
+
     try {
-        
-        // return success message
-    } catch (error) {
-        // return database connection error
-    }
-} catch (error) {
-    // if connection or no repo error
-    // return fetching error
-}
-}
+        const response = await fetch(readmeLink);
+        if (!response.ok) {
+            throw new Error(`GitHub API responded with status ${response.status}`);
+        }
 
+        const data = await response.json();
+        const readme = atob(data.content);
+
+        try {
+            
+            const result = await db.query(
+                `WITH user_data AS (
+                    SELECT id AS user_id FROM users WHERE github_username = $1
+                  )
+                  INSERT INTO readmes (readme, user_id)
+                  SELECT $2, user_id FROM user_data
+                  RETURNING *;`,
+                [username ,readme]
+            );
+            return { result: result, message: "successfully added to db"};
+        } catch (error) {
+            return { error: error, message: "Cannot connect to db" };
+        }
+    } catch (error) {
+        return { error: error, message: "Cannot connect to GitHub API" };
+    }
+} 
+
+
+
+// to test this function use this endpoint in api.js file 
+
+// router.get("/fetchReadme", async (_, res) => {
+// 	const test = await fetchReadme("RbAvci")
+// 	res.send(test)
+// });
