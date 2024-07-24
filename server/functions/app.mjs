9029264066ef28ecd54552a366925851bab52f9b
RbAvci <rb.avci@gmail.com>
2024-07-24 21:05:23 +0100

    add netlify deployment settings

---
+++ server/functions/app.mjs
@@ -0,0 +1,11 @@
+// Netlify wrapper for express.js
+import express from "express";
+import serverless from "serverless-http";
+import apiRouter from "../api";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/", apiRouter);
+
+export const handler = serverless(app);
