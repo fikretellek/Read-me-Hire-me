e9a2f12ba6e577632ab5e903568b4952d84ff355
RbAvci <rb.avci@gmail.com>
2024-07-26 01:43:57 +0100

    implemented footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
 import Header from "./components/AppHeader/AppHeader";
+import Footer from "./components/Footer/Footer";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
@@ -10,18 +11,27 @@ import { Protected } from "./pages/Protected";
 
 
 const App = () => (
-	  <>
-	<Header />
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-		<Route path="/signup" element={<SignUp />} />
-		<Route path="/signIn" element={<SignIn />} />
-		{/* <Route path="/unauthorized" element={<Unauthorized />} /> */}
-		{/* <Route path="/grad-dashboard" element={<Protected element={GradDashboard} roles={["graduate", "mentor", "recruiter"]} />} /> */}
-		<Route path="/update-password" element={<Protected element={UpdatePasswordForm} roles={["graduate", "mentor", "recruiter"]} />} />
-	</Routes>
-</>
+	<>
+		<Header />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about/this/site" element={<About />} />
+			<Route path="/signup" element={<SignUp />} />
+			<Route path="/signIn" element={<SignIn />} />
+			{/* <Route path="/unauthorized" element={<Unauthorized />} /> */}
+			{/* <Route path="/grad-dashboard" element={<Protected element={GradDashboard} roles={["graduate", "mentor", "recruiter"]} />} /> */}
+			<Route
+				path="/update-password"
+				element={
+					<Protected
+						element={UpdatePasswordForm}
+						roles={["graduate", "mentor", "recruiter"]}
+					/>
+				}
+			/>
+		</Routes>
+		<Footer />
+	</>
 );
 
 export default App;
