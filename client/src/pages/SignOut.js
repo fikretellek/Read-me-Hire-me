bd6b9f1776476af152ca6e1e9f1c861301909960
RbAvci <rb.avci@gmail.com>
2024-08-09 01:32:08 +0100

    fix css

--- client/src/pages/SignOut.js
+++ client/src/pages/SignOut.js
@@ -2,27 +2,39 @@ import { useNavigate } from "react-router-dom";
 import "./SignOut.css";
 
 const SignOut = ({ setSignedIn }) => {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const handleSignOut = () => {
-        localStorage.removeItem("token");
-        setSignedIn(false);
-        navigate("/signIn");
-    };
+	const handleSignOut = () => {
+		localStorage.removeItem("token");
+		setSignedIn(false);
+		navigate("/signIn");
+	};
 
-    const handleNo = () => {
-        navigate(-1);
-    };
+	const handleNo = () => {
+		navigate(-1);
+	};
 
-    return (
-			<div className="signOut">
-				<h1>Are you sure you want to sign out?</h1>
-				<div className="signOutButtons" >
-					<button onClick={handleSignOut}>Yes</button>
-					<button  className="noButton" onClick={handleNo}>No</button>
-				</div>
+	return (
+		<div className="signOut">
+			<h1>Are you sure you want to sign out?</h1>
+			<div className="signOutButtons">
+				<button
+					className="yesButton"
+					onClick={handleSignOut}
+					aria-label="Confirm sign out"
+				>
+					Yes
+				</button>
+				<button
+					className="noButton"
+					onClick={handleNo}
+					aria-label="Cancel sign out"
+				>
+					No
+				</button>
 			</div>
-		);
+		</div>
+	);
 };
 
 export default SignOut;
