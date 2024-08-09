d500ba40ef65bf41025ae60337a25a77591d4c6b
fikretellek <fikretellek@icloud.com>
2024-08-09 14:48:11 +0100

    contributions part updated

--- client/src/components/Contribution/Contribution.js
+++ client/src/components/Contribution/Contribution.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { parseISO, differenceInDays } from "date-fns";
 import "./contribution.css";
 
 const Contribution = ({ userId }) => {
@@ -9,7 +10,7 @@ const Contribution = ({ userId }) => {
 		const fetchContribution = async () => {
 			const token = localStorage.getItem("token");
 			try {
-				const response = await fetch(`/api/info/${userId}/activity`, {
+				const response = await fetch(`/api/info/${userId}/pullRequests`, {
 					headers: {
 						Authorization: `Bearer ${token}`,
 					},
@@ -18,8 +19,8 @@ const Contribution = ({ userId }) => {
 					throw new Error("Activity not found");
 				}
 				const result = await response.json();
-
-				setActivity(result.activity);
+				console.log(result);
+				setActivity(result);
 			} catch (error) {
 				setError(error.message);
 			}
@@ -34,30 +35,135 @@ const Contribution = ({ userId }) => {
 	if (Object.keys(activities).length === 0) {
 		return <div className="Activity-container">There is no Activity</div>;
 	}
+	const radius = 40;
+	const circumference = 2 * Math.PI * radius;
+	const offset =
+		circumference -
+		((activities.total - activities.merged_count) / 100) * circumference;
+
+	const now = new Date();
+	const intervals = {
+		last30Days: 0,
+		between30and60Days: 0,
+		between60and90Days: 0,
+	};
+	activities.pr_dates_array.forEach((date) => {
+		const daysAgo = differenceInDays(now, parseISO(date));
+
+		if (daysAgo <= 30) {
+			intervals.last30Days += 1;
+		} else if (daysAgo > 30 && daysAgo <= 60) {
+			intervals.between30and60Days += 1;
+		} else if (daysAgo > 60 && daysAgo <= 90) {
+			intervals.between60and90Days += 1;
+		}
+	});
+
+	const maxValue = Math.max(...Object.values(intervals));
+
+	console.log(intervals);
 
 	return (
 		<div className="activity-table">
-			<h1>Contributions</h1>
-			<table>
-				<thead>
-					<tr>
-						<th>Production</th>
-						<th>Documentation</th>
-						<th>Collaboration</th>
-						<th>Total</th>
-						<th>PR Dates</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>{activities.production}</td>
-						<td>{activities.documentation}</td>
-						<td>{activities.collaboration}</td>
-						<td>{activities.total}</td>
-						<td>{activities.pr_dates}</td>
-					</tr>
-				</tbody>
-			</table>
+			<div>
+				<p>Total PR's (90 days):</p>
+				<svg width="100" height="100" viewBox="0 0 100 100">
+					{/* outer circle */}
+					<circle
+						cx="50"
+						cy="50"
+						r={1}
+						stroke="#357abd"
+						strokeWidth="95"
+						fill="none"
+					/>
+					<text
+						x="50"
+						y="50"
+						textAnchor="middle"
+						fontSize="30"
+						dominantBaseline="middle"
+						fill="white"
+						fontFamily="Arial"
+					>
+						{activities.total}x
+					</text>
+				</svg>
+			</div>
+			<div>
+				<p>Merged PR's rate:</p>
+				<svg width="100" height="100" viewBox="0 0 100 100">
+					{/* outer circle */}
+					<circle
+						cx="50"
+						cy="50"
+						r={radius}
+						stroke="lightgray"
+						strokeWidth="15"
+						fill="none"
+					/>
+
+					{/* merged percentage circle */}
+					<circle
+						cx="50"
+						cy="50"
+						r={radius}
+						stroke="#35bd57"
+						strokeWidth="15"
+						fill="none"
+						strokeDasharray={circumference}
+						strokeDashoffset={circumference - offset}
+						transform="rotate(-90 50 50)"
+					/>
+					<text
+						x="50"
+						y="50"
+						textAnchor="middle"
+						fontSize="20"
+						dominantBaseline="middle"
+						fill="black"
+						fontFamily="Arial"
+					>
+						%{Math.floor((activities.merged_count / activities.total) * 100)}
+					</text>
+				</svg>
+			</div>
+			<div className="chart">
+				<div className="months-container">
+					<p id="left-legend" className="legend-type">pr counts</p>
+					<div
+						id="third-m"
+						className="month"
+						style={{
+							height: `${(intervals.between60and90Days / maxValue) * 100}%`,
+						}}
+					>
+						<p className="rate">x{intervals.between60and90Days}</p>
+					</div>
+					<div
+						id="second-m"
+						className="month"
+						style={{
+							height: `${(intervals.between30and60Days / maxValue) * 100 + 1}%`,
+						}}
+					>
+						<p className="rate">x{intervals.between30and60Days}</p>
+					</div>
+					<div
+						id="first-m"
+						className="month"
+						style={{ height: `${(intervals.last30Days / maxValue) * 100}%` }}
+					>
+						<p className="rate">x{intervals.last30Days}</p>
+					</div>
+					<p id="bottom-legend" className="legend-type">months</p>
+				</div>
+				<div className="legend">
+					<p>month before</p>
+					<p>last month</p>
+					<p>this month</p>
+				</div>
+			</div>
 		</div>
 	);
 };
--- server/controller/getPullRequests.js
+++ server/controller/getPullRequests.js
@@ -2,12 +2,14 @@ import db from "../db";
 
 export default async function getPullRequests(req, res) {
 	const userId = req.params.id;
+	console.log(userId)
 	try {
 		const userCheckQuery = `
         SELECT 
             COUNT(*) AS total, 
             COUNT(CASE WHEN state = 'MERGED' THEN 1 END) AS merged_count, 
-            COUNT(CASE WHEN created_at >= NOW() - INTERVAL '1 week' THEN 1 END) AS created_last_week
+            COUNT(CASE WHEN created_at >= NOW() - INTERVAL '1 week' THEN 1 END) AS created_last_week,
+			ARRAY_AGG(created_at) AS pr_dates_array
         FROM pull_requests
         WHERE user_id = $1;
         `;
